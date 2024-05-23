ccf0225c494f8ab89a3a094d4a5e451bcaaeb39b
TAdrianD <tadriand12@gmail.com>
2024-05-23 21:43:04 +0200

    GET

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+let adatok = [];
+
+fetch("https://pizza.kando-dev.eu/Pizza", {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        adatok.push(data[i]);
+        document.getElementById("pizzas").innerHTML += `<div class="menu-item">
+        <img src="${data.kepURL}" alt="Pizza 1">
+        <h3>${data.name}</h3>
+        <p>Egy sajtos pizza friss paradicsommal és bazsalikommal. Az olasz konyha klasszikus ízeivel.</p>
+      </div>`;
+    }
+});
+
+function Reszletek(id) {
+    console.log(id);
+
+    fetch(`https://pizza.kando-dev.eu/Pizza/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        document.getElementById("pizzas").innerHTML = `<div class="menu-item">
+        <img src="${data.kepURL}" alt="Pizza 1">
+        <h3>${data.name}</h3>
+        <p>Egy sajtos pizza friss paradicsommal és bazsalikommal. Az olasz konyha klasszikus ízeivel.</p>
+      </div>`;
+    });
+}
+
 fetch("https://pizza.kando-dev.eu/Pizza", {
     method: 'GET',
     headers: {
@@ -17,4 +62,3 @@ fetch("https://pizza.kando-dev.eu/Pizza", {
       </div>`;
      }
     })
-
